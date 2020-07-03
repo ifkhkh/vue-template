155a7372c76e50cc34ff0a6467f5d72ea8c066f4
ckk_hs_mac <hsckk@HsPrintdeMacBook-Pro.local>
2020-07-03 14:35:55 +0800

    tokens_practise a2

--- tokens_practise/a1.js
+++ tokens_practise/a1.js
@@ -6,19 +6,16 @@ let code = "abc 12 234 asjf931 asdf"
 
 const isNumber = str => Boolean(Number(str))
 
-const findNextIndex = (str, i) => {
+const findNextIndex = (str, i, symbolChar) => {
     for (let index = i; index < str.length; index++) {
         const c = str[index];
-        if (c === ' ') {
+        if (c === symbolChar) {
             return index
         }
     }
 }
 
 const numbers = function(code) {
-    let s = code
-    let i = 0
-
     // 用下标 i 循环遍历字符串 s
     // 如果 s[i] 是空格则往前走
     // 如果是字母，就调用自己实现的 stringEnd(s, i) 函数找到并返回空格的下标
@@ -30,21 +27,32 @@ const numbers = function(code) {
     // 字符串结尾可能不是空格（比如这里的 asdf）
     // 最简单的做法是，先在末尾加个空格再解析
     // 你知道很多编译器必须要求最后一个字符是回车键吗？
-
+    let s = code
+    let i = 0
     s = s + ' '
+    // 补充末尾空格便于遍历
     const r = []
-
     while (i < s.length) {
         const c = s[i]
         if (c === ' ') {
+            // 空格跳过
             i = i + 1            
         } else {
-            const nextIndex = findNextIndex(s, i)
+            // 找到下一个空格
+            const nextIndex = findNextIndex(s, i, ' ')
+            // 截取字符串
             const word = s.slice(i, nextIndex)
+            // 留下数字的
             isNumber(word) && r.push(word)
             i = nextIndex + 1
         }
     }
     return r
 }
-console.log(numbers(code))
+
+// console.log(numbers(code))
+
+module.exports = {
+    isNumber,
+    findNextIndex,
+} 
--- tokens_practise/a2.js
+++ tokens_practise/a2.js
@@ -1,3 +1,8 @@
+const {
+    isNumber,
+    findNextIndex,
+} = require('./a1')
+
 let code = "abc d efg 1 3 'gua' 'lang' ( )[]"
 // 这次我们增加 3 个功能
 // 0, 字符串也要能解析出来
@@ -7,14 +12,53 @@ let code = "abc d efg 1 3 'gua' 'lang' ( )[]"
 // log(tokens1(code))
 // ["abc", "d", "efg", "gua", "lang", "(", ")", "[", "]"]
 
-
+const log = console.log.bind(console)
 const tokens1 = function(code) {
-    let s = code
-    let i = 0
-
     // 上一题已经实现寻找数字和字符串结尾下标的函数，这里就是多了一个单引号字符串而已
     // 怎么办呢？
     // 无非是给单引号包裹的字符串增加一个特例，找到一个单引号的下标
     // 如果是单引号，就调用自己实现的 quoteStringEnd(s, i) 函数找到并返回单引号下标 + 1
     // 如果是 ()[] 这四个符号就直接加到结果数组中
+    let s = code
+    let i = 0
+    s = s + ' '
+    // 补充末尾空格便于遍历
+    const r = []
+    const specialSymbol = `()[]`
+    while (i < s.length) {
+        const c = s[i]
+        if (c === ' ') {
+            // 空格跳过
+            i = i + 1            
+        } else if (specialSymbol.indexOf(c) !== -1) {
+            // 特殊符号处理
+            r.push(c)
+            i = i + 1
+        } else if (c === "'") {
+            log(c, 'c')
+            const nextIndex = findNextIndex(s, i + 1, "'")
+            const word = s.slice(i + 1, nextIndex)
+            r.push(word)
+            log(nextIndex, word)
+            i = nextIndex + 1
+        } else {
+            // 找到下一个空格
+            const nextIndex = findNextIndex(s, i, ' ')
+            // 截取字符串
+            const word = s.slice(i, nextIndex)
+            // 留下数字的
+            // isNumber(word) && r.push(word)
+            r.push(word)
+            i = nextIndex + 1
+        }
+    }
+    return r
 }
+
+const __main = () => {
+
+    console.log(tokens1(code))
+}
+
+
+__main()
